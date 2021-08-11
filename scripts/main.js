3bb348b565da74f51c23326204904ae4d35932d9
LucaSantosSP <lucas-santos20.17@hotmail.com>
2021-08-11 09:42:06 -0300

    Resolução dos exercícios 2 e 3

--- scripts/main.js
+++ scripts/main.js
@@ -5,5 +5,23 @@ function maiorNum(lista){
             numero = lista[i];
         }
     }
+    document.write("Maior número da função: ")
     return numero;
+}
+
+function mediaArit(lista){
+    var media = 0;
+    for (var i in lista){
+        media = media + lista[i];
+    }
+    media = media/lista.length;
+    document.write("<br>" + "Média aritmética: ")
+    return media;
+}
+
+function tamanhoString(lista){
+    for (var i in lista){
+        document.write("<br>" + "String N" + i + ": ")
+        document.write(lista[i].length)
+    }
 }
