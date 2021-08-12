225132cdac1cdce3ea3abf08610e164815a3c976
LucaSantosSP <lucas-santos20.17@hotmail.com>
2021-08-12 13:40:40 -0300

    Resolução dos exercícios 4 e 6

--- scripts/main.js
+++ scripts/main.js
@@ -15,13 +15,40 @@ function mediaArit(lista){
         media = media + lista[i];
     }
     media = media/lista.length;
-    document.write("<br>" + "Média aritmética: ")
+    document.write("<br> Média aritmética: ")
     return media;
 }
 
 function tamanhoString(lista){
     for (var i in lista){
-        document.write("<br>" + "String N" + i + ": ")
+        document.write("<br> String N" + i + ": ")
         document.write(lista[i].length)
     }
+}
+
+function tamanhoString2(lista){
+    var quantidade = [];
+    for (var i in lista){
+        var x = lista[i].length
+        quantidade.push(x);
+    }
+    document.write("<br> Tamanho das strings: ")
+    return quantidade;
+}
+function ordemReversa(lista){
+    var reverso = [];
+    for (var i in lista){
+        reverso.unshift(lista[i])
+    }
+    document.write("<br> Lista em ordem reversa: ")
+    return reverso;
+}
+
+function nElementos(n, arr){
+    var retorno = [];
+    for (var i in n){
+        retorno.push(arr[i])
+    }
+    document.write("<br> Primeiros " + n + " elementos da lista: ")
+    return retorno;
 }
